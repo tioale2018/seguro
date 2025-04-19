56b069dc3fde5e10719ea746e41e191524c6122c
tioale2018 <ale.tpd@gmail.com>
2025-04-19 00:19:08 -0300

    atual

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<h1>Teste</h1>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="processa.php" method="post">
+    Nome: <input type="text" name="nome" required><br>
+    E-mail: <input type="email" name="email" required><br>
+    <input type="submit" value="Enviar">
+</form>
+    
+</body>
+</html>
---
+++ processa.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Olá, Mundo!</h1>
+    <?php
+    // Verifica se o formulário foi enviado
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Captura os dados do formulário
+        $nome = htmlspecialchars($_POST['nome']);
+        $email = htmlspecialchars($_POST['email']);
+
+        // Exibe os dados capturados
+        echo "<p>Nome: $nome</p>";
+        echo "<p>E-mail: $email</p>";
+    } else {
+        // echo "<p>Nenhum dado enviado.</p>";
+        // Redireciona para o formulário se não houver dados
+        //mostra um alerta e redireciona para o index.php
+        echo "<script>alert('Nenhum dado enviado.');</script>";
+        header("Location: index.php");
+        exit();
+    }
+
+
+    ?>
+    
+</body>
+</html>
